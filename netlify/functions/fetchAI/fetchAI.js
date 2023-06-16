aa2e43ba62d2164d5d08e899f59f573d5d8c13c9
Guangyao (Dave) Li <dave_guangyao_li@outlook.com>
2023-06-16 12:38:28 -0700

    set up serverless function

--- netlify/functions/fetchAI/fetchAI.js
+++ netlify/functions/fetchAI/fetchAI.js
@@ -1,17 +1,31 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY, // will fetch from netlify env variable
+})
+// delete configuration.baseOptions.headers['User-Agent'];
+const openai = new OpenAIApi(configuration)
+
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const response = await openai.createCompletion({
+      model: "davinci:ft-dave-gy-li-2023-06-16-07-11-13",
+      prompt: event.body, //conversationStr is being sent from the client side in the body of the fetch request, we can access it here using event.body
+      presence_penalty: 0,
+      frequency_penalty: 0.3,
+      max_tokens: 100,
+      stop: ['\n', '->']
+    })
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
+      body: JSON.stringify({
+        //a dd a key value pair. The key should be 'reply' and the value should be response.data.
+        reply: response.data,
+      }),
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
-module.exports = { handler }
+module.exports = { handler } // this means we can import this function in other files
